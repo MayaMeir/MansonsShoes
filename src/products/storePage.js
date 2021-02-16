924f654b1c4ec4e936ea87bab0a9665a9b2b280d
MayaMeir <themayameir@gmail.com>
2021-02-16 12:07:07 +0200

    sorting

--- src/products/storePage.js
+++ src/products/storePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OneProduct from './oneProduct';
 import { Container, Col, Row, Form } from 'react-bootstrap';
 
@@ -6,11 +6,35 @@ import '../theme/theme.css';
 
 function StorePage(props) {
 
+  let array = props.products;
+    let [prods, setSortProds] = useState(array);
+
+    function ascend(){
+      const sortedProds = prods.sort(function(a, b){return a.price - b.price})
+      setSortProds([...sortedProds]);
+
+  }
+
+  function desc(){
+    const sortedProds = prods.sort(function(a, b){return b.price - a.price})
+    setSortProds([...sortedProds]);
+
+  }
+  const handleChanges = (selectedItem) => {
+    console.log(selectedItem.target.value);
+    if (selectedItem.target.value == 'Price High To Low'){
+      desc();
+    }
+
+    if (selectedItem.target.value == 'Price Low To High'){
+      ascend();
+    }
+  }
 
     return <>
         <Container>
            { props.products.map((element, index) => {
-               <Col xs={3}><OneProduct product={props.products[index]} /></Col>
+               <Col xs={3}><OneProduct product={element} /></Col>
            })}
         <Row style={{marginTop:"7px"}}>
                 
@@ -18,7 +42,7 @@ function StorePage(props) {
                 <Col xs={4}></Col>
                 <Col xs={3}><Form style={{width:"150px", fontSize:"16px"}}>
   <Form.Group controlId="exampleForm.SelectCustom">
-    <Form.Control as="select" custom>
+    <Form.Control onChange={handleChanges} as="select" custom>
       <option>Top Rated</option>
       <option>Price High To Low</option>
       <option>Price Low To High</option>
