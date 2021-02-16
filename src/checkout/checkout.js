b3a09e9ba608f37a980f8d7cc48d127604abb5aa
MayaMeir <themayameir@gmail.com>
2021-02-16 16:39:40 +0200

    added checkout

--- src/checkout/checkout.js
+++ src/checkout/checkout.js
@@ -1,12 +1,64 @@
 import React from 'react';
-
-import {Button } from 'react-bootstrap';
+import './checkout.css';
+import {Button , Form, Row, Col, Container,InputGroup, FormControl } from 'react-bootstrap';
 
 function Checkout(){
 
 
     return <>
-    <h1>Checkout - Under Construction</h1>
+    <Container>
+
+
+  <Row>
+    <Col>
+    <Form style={{width:"85%", textAlign:"left"}}>
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridName">
+      <Form.Control type="text" placeholder="First Name" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridLastName">
+      <Form.Control type="text" placeholder="Last Name" />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group controlId="formGridAddress1">
+    <Form.Control placeholder="Address Line 1" />
+  </Form.Group>
+
+  <Form.Group controlId="formGridAddress2">
+    <Form.Control placeholder="Address Line 2" />
+  </Form.Group>
+
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Control placeholder="City" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridState">
+      <Form.Control as="select" defaultValue="Country">
+        <option>Country</option>
+        <option>...</option>
+      </Form.Control>
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridZip">
+      <Form.Control placeholder="Zip Code"/>
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group id="formGridCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+    </Col>
+    <Col>2 of 2</Col>
+  </Row> 
+</Container>
     </>
 }
 
--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,11 +1,39 @@
 import React from 'react';
-
-
+import bg from './gossip.jpg';
+import './contact.css';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 function Contact(){
 
-
     return <>
-    <h1>Contact Page - Under Construction</h1>
+            <img src={bg} alt="bg" id="bg"/>
+<Row style={{width:"90%", marginTop:"30px"}}>
+    <Col style={{marginTop:"60px"}}>
+    <h1>Reach Out To Us!</h1>
+    <h3>Something good? bad? You just want to gossip a bit?</h3>
+    <h3>Hit us up! </h3>
+
+    </Col>
+    <Col>
+    <div>
+    <Form id="contact-form" >
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" aria-describedby="emailHelp"  />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <textarea className="form-control" rows="5"  />
+          </div>
+          <Button variant="outline-dark" id="btn">Let's Talk!</Button>
+
+        </Form>
+    </div></Col>
+
+    </Row>
     </>
 }
 
