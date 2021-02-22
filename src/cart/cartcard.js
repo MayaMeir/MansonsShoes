cfb7818cfb68597919860ec4d91902c254c1d4c0
MayaMeir <themayameir@gmail.com>
2021-02-22 14:42:18 +0200

    changes

--- src/cart/cartcard.js
+++ src/cart/cartcard.js
@@ -1,32 +1,48 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, ButtonGroup } from 'react-bootstrap';
 import HoverImage from "react-hover-image";
 import src from '../products/productPics/smilyBoots/0TNAnKFNOQEvHcxzOc9UZHGjKeZHtJQu-33.jpg';
 
-function CartCard(props){
-    return(   <>
-    <div className="card mb-3" style={{width: '500px', textAlign:"left"}}>
+
+function CartCard(props) {
+
+  let [counter, setCount] = useState(1);
+
+  function add() {
+    setCount(counter + 1);
+  }
+  function remove() {
+    setCount(counter - 1);
+  }
+
+
+  return (<>
+    <div className="card mb-3" style={{ width: '500px', textAlign: "left" }}>
       <div className="row no-gutters">
         <div className="col-md-4">
-        <HoverImage id="prodImg" src={props.product.mainPicSrc} hoverSrc={src} style={{height: '220px'}}/>
+          <HoverImage id="prodImg" src={props.product.mainPicSrc} hoverSrc={src} style={{ height: '220px' }} />
         </div>
         <div className="col-md-8">
           <div className="card-body">
             <h2 className="card-title">{props.product.name}</h2>
-            <p className="card-text">
-              Quantity: Not Yet
-            </p>
+            <div className="card-text">
+              <ButtonGroup aria-label="Basic example" id="quantity">
+                <Button variant="secondary" onClick={remove}>-1</Button>
+                <Button variant="secondary" disabled>{counter}</Button>
+                <Button variant="secondary" onClick={add}>+1</Button>
+              </ButtonGroup>
+            </div>
             <p className="card-text">
               <small className="text-muted">{props.product.price}$</small>
-              <Button variant="dark" style={{float:"right"}} onClick={ () => props.onRemoveItem(props.index)}>Remove</Button>
+              <Button variant="dark" style={{ float: "right" }} onClick={() => props.onRemoveItem(props.index)}>Remove</Button>
             </p>
           </div>
         </div>
       </div>
     </div>
-    </>)
- 
-    
+  </>)
+
+
 }
 
 export default CartCard;
